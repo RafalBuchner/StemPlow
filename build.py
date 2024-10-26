9755d582997dcb69ef473fb7c37cbafd0e2c39c8
Rafał Buchner <rafal.buchner@gmail.com>
2024-10-26 15:32:47 +0200

    build file update

--- build.py
+++ build.py
@@ -1,19 +1,32 @@
-'''build RoboFont StemPlow Extension'''
+"""build RoboFont StemPlow Extension"""
 
 import os
-from mojo.extensions import ExtensionBundle
+import tomllib
+from pathlib import Path
 
-__version__ = "1.221"
+from mojo.extensions import ExtensionBundle  # type: ignore
+
+
+def getVersionFromPyProject():
+    pyprojectPath = Path(__file__).parent / "pyproject.toml"
+    pyProjectMetadata = tomllib.loads(pyprojectPath.read_text(encoding="utf-8"))
+    return pyProjectMetadata["tool"]["poetry"]["version"]
+
+
+__version__ = getVersionFromPyProject()
+
 
 def exec_cmd(cmd):
     import subprocess
     import shlex
     import traceback
+
     try:
         subprocess.check_call(shlex.split(cmd))
     except:
         print(f"issue with cmd\n\t${cmd}")
         print(traceback.format_exc())
+
 
 name = "Stem Plow"
 
@@ -21,13 +34,13 @@ name = "Stem Plow"
 basePath = os.path.dirname(__file__)
 
 # source folder for all extension files
-sourcePath = os.path.join(basePath, 'source')
+sourcePath = os.path.join(basePath, "source")
 
 # folder with python files
-libPath = os.path.join(sourcePath, 'code')
+libPath = os.path.join(sourcePath, "code")
 
 # # folder with html files
-htmlPath = os.path.join(sourcePath, 'documentation')
+htmlPath = os.path.join(sourcePath, "documentation")
 # htmlPath = None
 
 # # folder with resources (icons etc)
@@ -36,7 +49,7 @@ resourcesPath = None
 
 # load license text from file
 # see choosealicense.com for more open-source licenses
-licensePath = os.path.join(basePath, 'licence.txt')
+licensePath = os.path.join(basePath, "licence.txt")
 
 # boolean indicating if only .pyc should be included
 pycOnly = False
@@ -45,7 +58,7 @@ pycOnly = False
 extensionFile = f'{name.replace(" ","")}.roboFontExt'
 
 # path of the compiled extension
-buildPath = os.path.join(basePath, 'build')
+buildPath = os.path.join(basePath, "build")
 extensionPath = os.path.join(buildPath, extensionFile)
 
 # initiate the extension builder
@@ -54,21 +67,20 @@ B = ExtensionBundle()
 # scripts which should appear in Extensions menu
 B.addToMenu = [
     {
-        'path' : 'stemPlow/StemPlowSettings.py',
-        'preferredName': 'Settings…',
-        'shortKey' : '',
+        "path": "stemPlow/StemPlowSettings.py",
+        "preferredName": "Settings…",
+        "shortKey": "",
     }
-
 ]
 
 # name of the extension
 B.name = name
 
 # name of the developer
-B.developer = 'Rafał Buchner'
+B.developer = "Rafał Buchner"
 
 # URL of the developer
-B.developerURL = 'http://github.com/rafalbuchner'
+B.developerURL = "http://github.com/rafalbuchner"
 
 # # extension icon (file path or NSImage)
 # imagePath = os.path.join(resourcesPath, 'icon.png')
@@ -81,15 +93,14 @@ B.version = __version__
 B.launchAtStartUp = True
 
 # script to be executed when RF starts
-B.mainScript = 'StemPlowTool.py'
+B.mainScript = "StemPlowTool.py"
 
 # does the extension contain html help files?
 B.html = True
 
 # minimum RoboFont version required for this extension
-B.requiresVersionMajor = '4'
-B.requiresVersionMinor = '2'
-
+B.requiresVersionMajor = "4"
+B.requiresVersionMinor = "2"
 
 
 # license for the extension
@@ -100,11 +111,9 @@ with open(licensePath, encoding="utf-8") as license:
 # B.expireDate = '2019-12-31'
 
 # compile and save the extension bundle
-print(f'building extension {B.name} version {__version__}...', end=' ')
-# B.save(extensionPath, libPath=libPath, htmlPath=htmlPath, resourcesPath=resourcesPath, pycOnly=["3.6", "3.7"])
+print(f"building extension {B.name} version {__version__}...", end=" ")
 B.save(extensionPath, libPath=libPath, htmlPath=htmlPath, resourcesPath=resourcesPath)
-B.save(extensionPath, libPath=libPath, htmlPath=htmlPath, resourcesPath=resourcesPath)
-print('done!')
+print("done!")
 
 # check for problems in the compiled extension
 print()
