af304b4b5c595992f755b277a295f5493856e5fc
Rafał Buchner <rafal.buchner@gmail.com>
2024-11-02 16:44:58 +0100

    updating update_library script

--- update_library.sh
+++ update_library.sh
@@ -5,11 +5,25 @@ git clone --depth 1 --filter=blob:none --sparse https://github.com/RafalBuchner/
 cd stemmath
 git sparse-checkout set stemmath
 
+version=$(curl -s https://raw.githubusercontent.com/RafalBuchner/stemmath/main/pyproject.toml | grep '^version' | cut -d '"' -f2)
+
 # Move the downloaded folder to the destination directory
-mv stemmath ../source/code/stemPlow
+rm -rf ../rb/mat/stemmath
+mv stemmath ../rb/mat/
 
 # Clean up
 cd ..
 rm -rf stemmath
 
 echo "Folder downloaded and moved to source/code/stemPlow"
+
+# Extract version number from pyproject.toml
+
+
+echo $version
+# Insert version number at the 11th line of __init__.py
+# echo "__version__ = '$version'" >> $PWD/rb/mat/stemmath/__init__.py
+
+sed -i '' '11i\
+__version__ = "'$version'"
+' $PWD/rb/mat/stemmath/__init__.py
