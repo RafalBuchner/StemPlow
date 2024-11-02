fac19e5829b221fc0d034cdb819d8beaffc65efd
Rafał Buchner <rafal.buchner@gmail.com>
2024-11-02 16:28:05 +0100

    cleanup update_library

--- update_library.sh
+++ update_library.sh
@@ -1,14 +1,5 @@
 # #!/bin/bash
 
-# # Define source and destination directories
-# SOURCE_DIR="stemmath/stemmath"
-# DEST_DIR="source/code/stemPlow"
-
-# # Copy the folder
-# cp -r "$SOURCE_DIR" "$DEST_DIR"
-
-# echo "Folder copied from $SOURCE_DIR to $DEST_DIR"
-
 # Download the specific folder from GitHub
 git clone --depth 1 --filter=blob:none --sparse https://github.com/RafalBuchner/stemmath.git
 cd stemmath
